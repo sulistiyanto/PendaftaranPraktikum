c34f39bd5d320dbd7668c218061b26053dc986a3
Sulistiyanto <sulistiyanto404@gmail.com>
2016-12-07 15:49:19 +0700

    update

--- app/src/main/java/id/sulistiyanto/pendaftaranpraktikum/RecyclerViewAdapter.java
+++ app/src/main/java/id/sulistiyanto/pendaftaranpraktikum/RecyclerViewAdapter.java
@@ -1,8 +1,15 @@
 package id.sulistiyanto.pendaftaranpraktikum;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by sulistiyanto on 07/12/16.
@@ -10,24 +17,45 @@ import android.view.ViewGroup;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
+
+    private List<Result> results;
+
+    public RecyclerViewAdapter(List<Result> results) {
+        this.results = results;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view, parent, false);
+        ViewHolder holder = new ViewHolder(v);
+
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        Result result = results.get(position);
+        holder.textViewNPM.setText(result.getNpm());
+        holder.textViewNama.setText(result.getNama());
+        holder.textViewKelas.setText(result.getKelas());
+        holder.textViewSesi.setText(result.getSesi());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return results.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.textNPM) TextView textViewNPM;
+        @BindView(R.id.textNama) TextView textViewNama;
+        @BindView(R.id.textKelas) TextView textViewKelas;
+        @BindView(R.id.textSesi) TextView textViewSesi;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
--- app/src/main/java/id/sulistiyanto/pendaftaranpraktikum/ViewActivity.java
+++ app/src/main/java/id/sulistiyanto/pendaftaranpraktikum/ViewActivity.java
@@ -2,12 +2,37 @@ package id.sulistiyanto.pendaftaranpraktikum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ViewActivity extends AppCompatActivity {
+
+    private List<Result> results = new ArrayList<>();
+    private RecyclerViewAdapter viewAdapter;
+
+    @BindView(R.id.recyclerView) RecyclerView recyclerView;
+    @BindView(R.id.progress_bar) ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        ButterKnife.bind(this);
+        getSupportActionBar().setTitle("Daftar Mahasiswa Praktikum");
+
+        viewAdapter = new RecyclerViewAdapter(results);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(viewAdapter);
+
     }
 }
