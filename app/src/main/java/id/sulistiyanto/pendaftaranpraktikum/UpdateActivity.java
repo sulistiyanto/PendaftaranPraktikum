a8219e926ee76c88d5fc897260ed7cc0f95019dd
sulistiyanto <sulistiyanto404@gmail.com>
2017-01-21 09:17:18 +0700

    Delete data

--- app/src/main/java/id/sulistiyanto/pendaftaranpraktikum/UpdateActivity.java
+++ app/src/main/java/id/sulistiyanto/pendaftaranpraktikum/UpdateActivity.java
@@ -1,9 +1,12 @@
 package id.sulistiyanto.pendaftaranpraktikum;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -103,11 +106,63 @@ public class UpdateActivity extends AppCompatActivity {
         }
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_delete, menu);
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
+                break;
+            case R.id.action_delete:
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle("Peringatan");
+                alertDialogBuilder
+                        .setMessage("Apakah Anda yakin ingin mengapus data ini?")
+                        .setCancelable(false)
+                        .setPositiveButton("Hapus",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+
+                                String npm = editTextNPM.getText().toString();
+                                Retrofit retrofit = new Retrofit.Builder()
+                                        .baseUrl(URL)
+                                        .addConverterFactory(GsonConverterFactory.create())
+                                        .build();
+                                RegisterAPI api = retrofit.create(RegisterAPI.class);
+                                Call<Value> call = api.hapus(npm);
+                                call.enqueue(new Callback<Value>() {
+                                    @Override
+                                    public void onResponse(Call<Value> call, Response<Value> response) {
+                                        String value = response.body().getValue();
+                                        String message = response.body().getMessage();
+                                        if (value.equals("1")) {
+                                            Toast.makeText(UpdateActivity.this, message, Toast.LENGTH_SHORT).show();
+                                            finish();
+                                        } else {
+                                            Toast.makeText(UpdateActivity.this, message, Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onFailure(Call<Value> call, Throwable t) {
+                                        t.printStackTrace();
+                                        Toast.makeText(UpdateActivity.this, "Jaringan Error!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton("Batal",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
                 break;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/id/sulistiyanto/pendaftaranpraktikum/ViewActivity.java
+++ app/src/main/java/id/sulistiyanto/pendaftaranpraktikum/ViewActivity.java
@@ -1,6 +1,5 @@
 package id.sulistiyanto.pendaftaranpraktikum;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -9,7 +8,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
